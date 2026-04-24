ead4c63d352975808d91838d37998882206a659d
Jinoo83S <jinwoosuk@his.sc.kr>
2026-04-24 13:51:57 +0900

    클루드 수정

--- db.js
+++
@@ -1,8 +0,0 @@
-
-const DB = {
-  teachers: () => db.ref("teachers"),
-  students: () => db.ref("students"),
-  entries: () => db.ref("entries"),
-  notices: () => db.ref("notices"),
-  recovery: () => db.ref("recovery")
-};
